0539fe19f48e3e0cd4e4e88a1a064e8572293d20
Hamid Ryahi <hamidryahi96@gmail.com>
2024-07-07 17:32:26 +0100

    add netlify.toml

--- server.js
+++ server.js
@@ -1,31 +1,46 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+
 // Require Express to run server and routes
 const express = require('express');
+const path = require('path');
+
 // Require Body-Parser
 const bodyParser = require('body-parser');
+
 // Require Cors
 const cors = require('cors');
+
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Cors for cross origin allowance
 app.use(cors());
+
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static(path.join(__dirname, 'website')));
+
+
 // Setup Server
 const port = 3001;
 const listening = () => {
   console.log(`Running on localhost ${port}`);
 }
+
 app.listen(port, listening);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'website', 'index.html'));
+});
 // GET route that returns the projectData object
 app.get('/allData', (req, res) => {
   res.send(projectData);
 });
+
 // POST route that adds incoming data to projectData
 app.post('/addData', (req, res) => {
   projectData['temperature'] = req.body.temperature;
